4f75746b7f18ae4ab6486e99513b2d47c56d3483
João Vitor Duarte Mariucio <joaovitorduartemariucio@gmail.com>
2021-07-22 17:54:57 -0300

    Teste do modulo CommonUtilities

--- ModularizationStudy/ViewController.swift
+++ ModularizationStudy/ViewController.swift
@@ -6,13 +6,40 @@
 //
 
 import UIKit
+import CommonUtilities
 
 class ViewController: UIViewController {
+    
+    let imageview: UIImageView = {
+        var image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
+        print(Bundle.appName)
+        print("teste".localizable)
+        
+        addLayout()
+        setImage()
+    }
+    
+    func addLayout() {
+        
+        view.addSubview(imageview)
+        NSLayoutConstraint.activate([
+            imageview.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageview.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageview.heightAnchor.constraint(equalToConstant: 200),
+            imageview.widthAnchor.constraint(equalToConstant: 250)
+        ])
+    }
+    
+    func setImage() {
+        imageview.load(with: "https://blog.emania.com.br/wp-content/uploads/2019/07/fotografo-capta-imagem-incrivel-do-eclipse-solar-no-chile-Blog-eMania-03-07.jpg")
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
--- Podfile
+++ Podfile
@@ -1,5 +1,6 @@
 # Uncomment the next line to define a global platform for your project
 platform :ios, '13.0'
+source 'https://github.com/CocoaPods/Specs'
 source 'https://github.com/joaoduartemariucio/my-portfolio-specs'
 
 target 'ModularizationStudy' do
@@ -8,7 +9,7 @@ target 'ModularizationStudy' do
 
   # Pods for ModularizationStudy
   pod 'CommonUtilities'
-
+  
   target 'ModularizationStudyTests' do
     inherit! :search_paths
     # Pods for testing
