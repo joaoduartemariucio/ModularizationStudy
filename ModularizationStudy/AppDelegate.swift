9d8d9894cd31be18a27faad0ac16500a79bda697
João Vitor Duarte Mariucio <joaovitorduartemariucio@gmail.com>
2021-07-22 10:59:30 -0300

    Base project

---
+++ ModularizationStudy/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  ModularizationStudy
+//
+//  Created by João Vitor Duarte Mariucio on 22/07/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
---
+++ ModularizationStudy/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ModularizationStudy
+//
+//  Created by João Vitor Duarte Mariucio on 22/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .darkContent
+    }
+}
+
